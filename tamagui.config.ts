76110a37c3e71df66c4ce3a1a287acd6e144b1ec
ammar1029 <ammarfakih02@gmail.com>
2023-09-18 14:26:35 -0700

    theme

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,17 +1,22 @@
-import { config } from '@tamagui/config'
-
-import { createTamagui } from 'tamagui'
-const tamaguiConfig = createTamagui(config)
+import { config } from "@tamagui/config";
+import { color, radius, size, space, themes, zIndex } from "@tamagui/themes";
+import { createTamagui, createTokens } from "tamagui";
+const tokens = createTokens({
+  size,
+  space,
+  zIndex,
+  color,
+  radius,
+});
+const tamaguiConfig = createTamagui({ ...config, themes, tokens });
 // this makes typescript properly type everything based on the config
 
-type Conf = typeof tamaguiConfig
+type Conf = typeof tamaguiConfig;
 
-declare module 'tamagui' {
-
+declare module "tamagui" {
   interface TamaguiCustomConfig extends Conf {}
-
 }
-export default tamaguiConfig
+export default tamaguiConfig;
 // depending on if you chose tamagui, @tamagui/core, or @tamagui/web
 
 // be sure the import and declare module lines both use that same name
